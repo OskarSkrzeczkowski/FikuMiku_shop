9eedd4cab1c27e93296d1336def4589b9454f3c4
Oskar <oskar.skrzeczkowski@gmail.com>
2026-04-24 12:35:23 +0200

    Przygotowanie plików pod nowe komponenty - Oskar Skrzeczkowski

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import { HeaderTop } from './header';
+import { HeaderTop } from '../navComponents/header';
 import { Footer } from './sections';
 
 /*Komponent który określa stały szablon strony*/
